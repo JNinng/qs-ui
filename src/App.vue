589298b9a16817290d0b96992eda39e2f2ebc32b
OhmLaw <xx1599xx@163.com>
2022-10-23 16:26:55 +0800

    🦄 refactor(App): 简化App组件

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
 <template>
   <div id="app">
-    <div class="menu">
-      <home-menu></home-menu>
-    </div>
-    <div class="content">
-      <router-view></router-view>
-    </div>
+    <main-x></main-x>
   </div>
 </template>
 
 <script>
-import HomeMenu from "@/components/HomeMenu";
+import MainX from "@/components/Main.vue";
 
 export default {
   name: "App",
   components: {
-    HomeMenu,
+    MainX,
   },
   data() {
-    return {
-      isVisible: true,
-    };
+    return {};
   },
   methods: {},
 };
@@ -28,15 +21,16 @@ export default {
 
 <style>
 * {
-  margin: 0;
-  padding: 0;
+	margin: 0;
+	padding: 0;
 }
 
 .menu {
-  width: 100%;
-  height: 59px;
-  margin-bottom: 16px;
-  align-items: center; /* 垂直居中 */
-  justify-content: center; /* 水平居中 */
+	width: 100%;
+	height: 59px;
+
+	align-items: center; /* 垂直居中 */
+	justify-content: center; /* 水平居中 */
 }
+
 </style>
---
+++ src/components/Main.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <div class="menu">
+      <home-menu></home-menu>
+    </div>
+    <div class="content">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import HomeMenu from "@/components/HomeMenu";
+
+export default {
+  name: "Main",
+
+  components: {
+    HomeMenu,
+  },
+
+  mixins: [],
+
+  props: {},
+
+  data() {
+    return {
+      isVisible: true,
+    };
+  },
+
+  computed: {},
+
+  watch: {},
+
+  created() {},
+
+  methods: {},
+
+  computed: {},
+
+  watch: {},
+
+  created() {},
+
+  methods: {},
+};
+</script>
+
+<style scoped>
+* {
+	margin: 0;
+	padding: 0;
+}
+
+.menu {
+	width: 100%;
+	height: 59px;
+
+	align-items: center; /* 垂直居中 */
+	justify-content: center; /* 水平居中 */
+}
+
+</style>
