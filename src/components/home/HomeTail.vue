3f2359b034531b67b7779c0cdf56916cb9ab9ce8
OhmLaw <xx1599xx@163.com>
2023-02-04 13:07:22 +0800

    ✨ feat(HomeTail): 底部增加备案信息

--- src/components/home/HomeTail.vue
+++ src/components/home/HomeTail.vue
@@ -1,13 +1,52 @@
 <template>
   <div class="root">
-    <p>tail</p>
-    <p>tail</p>
-    <p>tail</p>
-    <p>tail</p>
-    <p>tail</p>
-    <p>tail</p>
-    <p>tail</p>
-    <p>tail</p>
+    <div style="width: 380px; margin: 0 auto; padding: 20px 0">
+      <p style="display: block-inline; text-align: center">
+        © 2021-2023 xxxx.xxx 版权所有
+      </p>
+      <a
+        href="https://beian.miit.gov.cn/"
+        target="_blank"
+        style="
+          display: inline-block;
+          float: left;
+          text-align: center;
+          text-decoration: none;
+          height: 20px;
+          width: 42%;
+          line-height: 20px;
+          color: #939393;
+        "
+        >粤ICP备2022060606号</a
+      >
+      <a
+        target="_blank"
+        href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=44152302000034"
+        style="
+          display: inline-block;
+          float: left;
+          text-decoration: none;
+          height: 20px;
+          width: 58%;
+          line-height: 20px;
+        "
+        ><img
+          src="@/assets/备案图标.png"
+          style="float: left; margin: 0px auto"
+        />
+        <p
+          style="
+            float: left;
+            height: 20px;
+            line-height: 20px;
+            margin: 0px 0px 0px 5px;
+            color: #939393;
+          "
+        >
+          粤公网安备 44060606060606号
+        </p></a
+      >
+    </div>
   </div>
 </template>
 
@@ -41,24 +80,28 @@ export default {
 
 <style scoped>
 * {
-	margin: 0;
-	padding: 0;
+  margin: 0;
+  padding: 0;
+  font-size: 13px;
 }
 
 p {
-	text-align: center;
+  text-align: center;
 }
 
 .root {
-	/* margin: 10px; */
-	padding: 30px 0 20px 0;
+  /* margin: 10px; */
+  padding: 30px 0 20px 0;
 
-	background-color: ivory;
+  background-color: ivory;
 
-/* box-shadow: rgba(0, 0, 0, .25) 0 14px 28px, rgba(0, 0, 0, .22) 0 10px 10px; */
+  /* box-shadow: rgba(0, 0, 0, .25) 0 14px 28px, rgba(0, 0, 0, .22) 0 10px 10px; */
 
-	align-items: center; /* 垂直居中 */
-	justify-content: center; /* 水平居中 */
+  align-items: center; /* 垂直居中 */
+  justify-content: center; /* 水平居中 */
 }
 
+p {
+  margin-bottom: 4px;
+}
 </style>
