ea3a595207ef696cece940465b4bfe86b57c6695
JNinng <jninng@163.com>
2024-03-12 18:48:18 +0800

    🐞 fix(config): ip,port 修正

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import qs from "qs";
 
 // axios.defaults.baseURL = "/api";
-const serverAddress = "http://192.168.0.105:8401";
+const serverAddress = "http://192.168.1.102:8401";
 axios.defaults.baseURL = serverAddress + "/";
 
 export default {
